4d10089e89ed6cd05ea2dc4a774db18b3ba63e2e
jandrepont <joelandrepont1@gmail.com>
2017-10-21 13:56:47 -0500

    added embedded h2 DB

---
+++ src/main/resources/db/migration/V001__Creature.sql
@@ -0,0 +1,6 @@
+create table creature (
+  id  	      BIGSERIAL PRIMARY KEY,
+  name       	VARCHAR(100),
+species       VARCHAR(100)
+
+)
---
+++ src/main/resources/db/migration/V002__InsertCreatureData.sql
@@ -0,0 +1,6 @@
+INSERT INTO creature ( name, species) VALUES
+  ('Alice', 'homo sapien'),
+  ('Bob', 'homo sapien'),
+  ('Stinky', 'felis catus'),
+  ('Sam', 'siliqua patula'),
+  ('Amy', 'ameoba proteus');
