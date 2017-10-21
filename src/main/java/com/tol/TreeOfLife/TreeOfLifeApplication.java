12ebcb5ea5e2af15acd493e09cf7e1b6e6e04f6b
jandrepont <joelandrepont1@gmail.com>
2017-10-21 11:28:52 -0500

    initial push

---
+++ src/main/java/com/tol/TreeOfLife/TreeOfLifeApplication.java
@@ -0,0 +1,12 @@
+package com.tol.TreeOfLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeOfLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeOfLifeApplication.class, args);
+	}
+}
